922cbe47b1a301a237135110feeb2efa5b363039
rotimiazeez <harzeyzhrotimi@gmail.com>
2021-11-04 10:54:56 -0700

    chore: Add linter configurations

--- morse_code.rb
+++ morse_code.rb
@@ -21,3 +21,15 @@ end
 def decode(char)
 
 end
+
+# def decode_morse(morse_code)
+#   morse_code
+#     .split('   ')
+#     .select do |char|
+#       char != ''
+#     end
+#     .map do |word|
+#       word.split(' ').map { |char| MORSE_CODE[char] }.join
+#     end
+#     .join(' ')
+# end
