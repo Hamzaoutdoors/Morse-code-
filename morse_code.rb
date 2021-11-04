30a6fb229372994c5ad991f1f4956ef166d70a4b
Hamzaoutdoors <ellaouzihamza@gmail.com>
2021-11-04 21:19:44 +0100

    fix : fix rubocop errors

--- morse_code.rb
+++ morse_code.rb
@@ -1,12 +1,12 @@
 def decode_char(char)
-    @morse_dict = { '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D',
-    '.' => 'E', '..-.' => 'F', '--.' => 'G', '....' => 'H',
-    '..' => 'I', '.---' => 'J', '-.-' => 'K', '.-..' => 'L',
-    '--' => 'M', '-.' => 'N', '---' => 'O', '.--.' => 'P',
-    '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
-    '..-' => 'U', '...-' => 'v', '.--' => 'W', '-..-' => 'X',
-    '-.--' => 'Y', '--..' => 'Z' }
-    @morse_dict.each do |key, value|
+  @morse_dict = { '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D',
+                  '.' => 'E', '..-.' => 'F', '--.' => 'G', '....' => 'H',
+                  '..' => 'I', '.---' => 'J', '-.-' => 'K', '.-..' => 'L',
+                  '--' => 'M', '-.' => 'N', '---' => 'O', '.--.' => 'P',
+                  '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
+                  '..-' => 'U', '...-' => 'v', '.--' => 'W', '-..-' => 'X',
+                  '-.--' => 'Y', '--..' => 'Z' }
+  @morse_dict.each do |key, value|
     return value if key == char
   end
 end
@@ -14,16 +14,16 @@ end
 def decode_word(word)
   new_char = ''
   full_word = ''
-  word_split = word.split(' ')
+  word_split = word.split
   word_split.each do |letter|
-    if letter.to_s == '/'
-      new_char = ' '
-    else
-      new_char = decode_char(letter.to_s)
-    end
+    new_char = if letter.to_s == '/'
+                 ' '
+               else
+                 decode_char(letter.to_s)
+               end
     full_word += new_char
   end
-  return full_word
+  full_word
 end
 
 def decode(msg)
@@ -32,7 +32,7 @@ def decode(msg)
   msg_split = msg.split('   ')
   msg_split.each do |word|
     new_word = decode_word(word)
-    full_message += new_word + ' '
+    full_message += "#{new_word} "
   end
   puts full_message
 end
